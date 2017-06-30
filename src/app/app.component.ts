c273ec718919288c1c8871717ac0c23a4837c062
AndreAraujo <aaraujorjo@gmail.com>
2017-06-29 21:35:28 -0300

    Primeiro Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component, ViewChild } from '@angular/core';
+import { Platform, MenuController, Nav } from 'ionic-angular';
+
+import { UserLogin } from '../pages/user-login/user-login';
+import { Dashboard } from '../pages/dashboard/dashboard';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  // make HelloIonicPage the root (or first) page
+  rootPage = UserLogin;
+  pages: Array<{title: string,icon:string, component: any}>;
+
+  constructor(
+    public platform: Platform,
+    public menu: MenuController,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen
+  ) {
+    this.initializeApp();
+
+    // set our app's pages
+    this.pages = [
+      { title: 'Home',icon:'home', component: Dashboard },
+      { title: 'Menu1',icon:'book', component: Dashboard },
+      { title: 'Menu2',icon:'bookmark', component: Dashboard },
+      { title: 'Menu3',icon:'bookmarks', component: Dashboard },
+      { title: 'Menu4',icon:'briefcase', component: Dashboard },
+      { title: 'Menu5',icon:'bulb', component: Dashboard },
+      { title: 'Menu6',icon:'cafe', component: Dashboard },
+      { title: 'Logout',icon:'lock', component: UserLogin }
+    ];
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
+  }
+}
